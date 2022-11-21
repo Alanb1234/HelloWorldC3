3eadc998c6238339e957b1ddc08d1d44e2fb53ee
Alan Binu <alanbinu2013@yahoo.co.uk>
2022-11-21 15:43:39 +0000

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,12 @@
+public class Program {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 1;
+        System.out.println(num1 == num2);
+        
+        System.out.println(num1 == num2);
+
+
+    }
+
+}
