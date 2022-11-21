a736a174146d1ea7c9da2a7d3c028a808f7cb35b
Alan Binu <alanbinu2013@yahoo.co.uk>
2022-11-21 17:21:33 +0000

    final commit

--- src/Program.java
+++ src/Program.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        // Initilse the username and password
-        String username = "TheCabage123";
-        String password = "banana@321";
+        // initialize the username and password
+        String username = "admin";
+        String password = "PaSSword!!!";
 
-        int i =1;
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
+        System.out.println("Enter username:");
+        String usernameInput = myObj.nextLine(); // Read username input
 
+        System.out.println("Enter password:");
+        String userPasswordInput = myObj.nextLine(); // Read username input
 
+        // Checks if the credentials entered are correct
+        if (usernameInput.toLowerCase().equals(username) && userPasswordInput.equals(password) ){
+            System.out.println("Access Granted");
 
+        }else{
+            System.out.println("Access Denied");
+
+        }
 
     }
 
