96ee892b94f3823507ca3b0c03d1462bb3652c34
Alan <alanbinu2013@yahoo.co.uk>
2022-11-25 16:04:32 +0000

    Update Program.java
     make the code repeat

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Program {
+public class Main {
     public static void main(String[] args) {
 
         // initialize the username and password
@@ -8,18 +8,28 @@ public class Program {
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-        System.out.println("Enter username:");
-        String usernameInput = myObj.nextLine(); // Read username input
+        int counter = 3;
 
-        System.out.println("Enter password:");
-        String userPasswordInput = myObj.nextLine(); // Read username input
+        while (counter > 0){
 
-        // Checks if the credentials entered are correct
-        if (usernameInput.toLowerCase().equals(username) && userPasswordInput.equals(password) ){
-            System.out.println("Access Granted");
+            System.out.println("Enter username:");
+            String usernameInput = myObj.nextLine(); // Read username input
 
-        }else{
-            System.out.println("Access Denied");
+            System.out.println("Enter password:");
+            String userPasswordInput = myObj.nextLine(); // Read username input
+
+            // Checks if the credentials entered are correct
+            if (usernameInput.toLowerCase().equals(username) && userPasswordInput.equals(password) ){
+                System.out.println("Access Granted");
+
+                break;
+
+            }else{
+                System.out.println("Access Denied");
+
+                counter--;
+
+            }
 
         }
 
